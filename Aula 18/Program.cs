ab2209ecb89bfc42c4a35da52468079d0346a477
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-06-03 13:04:03 -0300

    Criado um sistema onde o usuario Cria uma pasta com o nome Notas, coloca o conteudo, le o conteudo, deleta o conteudo e pode gerar um novo conteudo.

--- Aula 18/Program.cs
+++ Aula 18/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace Aula18;
 using System.IO;
+using System.Transactions;
+
 public class Program
 {
     public static void Main()
@@ -20,21 +22,94 @@ public class Program
 
         //3. Inserir um paragráfo em um arquivo
 
-        string path = @"C:\Users\daniel.ferreira\Documents\GitHub\C#\Curso de C#\Aula 18\";
-        string fileName = "MyDocument.Doc";
-        string fullPath = path + fileName;
-        string content = "Hello World from a specific path!";
-        string additionalContent = "Curso de C# e .NET";
+        //string path = @"C:\Users\daniel.ferreira\Documents\GitHub\C#\Curso de C#\Aula 18\";
+        //string fileName = "MyDocument.Doc";
+        //string fullPath = path + fileName;
+        //string content = "Hello World from a specific path!";
+        //string additionalContent = "Curso de C# e .NET";
 
-        File.AppendAllText(fullPath, additionalContent);
+        //File.AppendAllText(fullPath, additionalContent);
 
-        Console.WriteLine("Arquivo Atual criado com suscesso");
+        //Console.WriteLine("Arquivo Atual criado com suscesso");
 
         //4. Ler o conteúdo de um arquivo
 
-        string fileContent = File.ReadAllText(fullPath);
-        Console.WriteLine("Conteúdo do arquivo:" + fileContent);
+        //string fileContent = File.ReadAllText(fullPath);
+        //Console.WriteLine("Conteúdo do arquivo:" + fileContent);
 
+        // testando um programa
+
+        //string path = (@"C:\Users\daniel.ferreira\Documents\GitHub\C#\Curso de C#\Aula 18\Notas\");
+        //string fileName = "Notas.txt";
+        //string fullPath = path + fileName;
+        //string content = "Notas dos alunos:\n";
+
+        //File.WriteAllText(fullPath, content);
+        string local = (@"C:\Users\daniel.ferreira\Documents\GitHub\C#\Curso de C#\Aula 18\Notas");
+        Directory.CreateDirectory(local);
+
+        while (true)
+        {
+             Console.WriteLine("----------------Criando um Bloco de notas Automatico---------------------");
+   
+            string path = (@"C:\Users\daniel.ferreira\Documents\GitHub\C#\Curso de C#\Aula 18\Notas\");
+            Console.Write("Qual nome do arquivo ?");
+            string fileName = Console.ReadLine();
+
+            string fullPath = path + fileName + ".txt";
+            Console.Write("Qual texto vai estar dentro do TXT ?");
+            string content = Console.ReadLine();
+
+            File.WriteAllText(fullPath, content);
+            Console.WriteLine("Arquivo criado com sucesso!");
+
+            Console.WriteLine("Deseja ler o conteudo dentro do arquivo ? (s/n)");
+            string readChoice = Console.ReadLine().ToUpper();
+            if (readChoice == "S")
+            {
+                string fileContent = File.ReadAllText(fullPath);
+                Console.WriteLine("Conteúdo do arquivo: " + fileContent);
+            }
+            else
+            {
+                Console.WriteLine("Você escolheu não ler o conteúdo do arquivo.");
+            }
+
+            Console.WriteLine("Deseja Excluir algum arquivo ? (s/n)");
+            string deleteChoice = Console.ReadLine().ToUpper();
+            if (deleteChoice == "S")
+            {
+                Console.Write("Digite o nome do arquivo que deseja excluir (sem extensão): ");
+                string deleteFileName = Console.ReadLine();
+                string deleteFullPasth = path + deleteFileName + ".txt";
+                if (File.Exists(deleteFullPasth))
+                {
+                    File.Delete(deleteFullPasth);
+                    Console.WriteLine("Arquivo excluído com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("Arquivo não encontrado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Você escolheu não excluir nenhum arquivo.");
+            }
+
+             Console.WriteLine("Deseja criar outro arquivo? (s/n)");
+            string choice = Console.ReadLine().ToUpper();
+            if (choice != "S")
+            {
+                Console.WriteLine("Programa encerrado.");
+                break;
+            }
+            else
+            {
+                Console.Clear();
+            }
+            
+        }
 
     }
 }
